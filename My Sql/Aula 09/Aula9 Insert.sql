2eb06b221b669c7575e961958bbc272755926d3e
Jadilson Junior <74326526+JadilsonJR@users.noreply.github.com>
2022-03-10 20:29:20 -0300

    Update Aula9 Insert.sql

--- My Sql/Aula 09/Aula9 Insert.sql
+++ My Sql/Aula 09/Aula9 Insert.sql
@@ -1,10 +1,13 @@
-insert into cliente_aux (i_cliente_cliente, s_nome_cliente, s_cpf_cliente, d_nasc_cliente, i_tipo_cliente)
-select 	
-i_cliente_cliente, 
-s_nome_cliente, 
-s_cpf_cliente,
- d_nasc_cliente, 
- i_tipo_cliente 
- from cliente;
+
+-- Inserindo todos os dados (de uma consulta Select) da tabela cliente para a tabela cliente_aux
+insert into cliente_aux 
+	(i_cliente_cliente, s_nome_cliente, s_cpf_cliente, d_nasc_cliente, i_tipo_cliente)
+	select 	
+		i_cliente_cliente, 
+		s_nome_cliente, 
+		s_cpf_cliente,
+		d_nasc_cliente, 
+		i_tipo_cliente 
+	from cliente;
  
- select * from cliente;
+ select * from cliente_aux;
